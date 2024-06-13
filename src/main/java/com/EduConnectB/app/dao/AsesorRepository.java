f8f961a277504df9d2656cdc95f8a3686a76dab7
HcVm <hecvega96@outlook.com>
2024-06-12 23:42:59 -0500

    Aprobaciones y rechazos

--- src/main/java/com/EduConnectB/app/dao/AsesorRepository.java
+++ src/main/java/com/EduConnectB/app/dao/AsesorRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.EduConnectB.app.models.Asesor;
+import com.EduConnectB.app.models.EstadoUsuario;
 import com.EduConnectB.app.models.Usuario;
 
 @Repository
@@ -15,5 +16,7 @@ public interface AsesorRepository extends JpaRepository<Asesor, Integer> {
 	List<Asesor> findByEspecialidad(String especialidad);
 	
 	Optional<Asesor> findByUsuario(Usuario usuario);
+	
+	List<Asesor> findByUsuarioEstado(EstadoUsuario estado);
 	
 }
--- src/main/java/com/EduConnectB/app/services/NotificacionService.java
+++ src/main/java/com/EduConnectB/app/services/NotificacionService.java
@@ -1,33 +1,41 @@
 package com.EduConnectB.app.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.EduConnectB.app.dao.NotificacionRepository;
-import com.EduConnectB.app.models.Notificacion;
+import com.EduConnectB.app.models.Asesor;
+import com.EduConnectB.app.models.TipoUsuario;
+import com.EduConnectB.app.models.Usuario;
+import com.EduConnectB.app.dao.UsuarioRepository;
 
 import java.util.List;
-import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
 @Service
 public class NotificacionService {
 
     @Autowired
-    private NotificacionRepository notificacionRepository;
+    private JavaMailSender mailSender;
 
-    public List<Notificacion> obtenerTodasLasNotificaciones() {
-        return notificacionRepository.findAll();
-    }
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
-    public Optional<Notificacion> obtenerNotificacionPorId(Integer idNotificacion) {
-        return notificacionRepository.findById(idNotificacion);
-    }
+    public void enviarNotificacionNuevoAsesor(Asesor asesor) {
+        List<String> correosAdministradores = usuarioRepository.findByTipoUsuario(TipoUsuario.ADMIN)
+                .stream()
+                .map(Usuario::getCorreoElectronico)
+                .toList();
 
-    public Notificacion guardarNotificacion(Notificacion mensaje) {
-        return notificacionRepository.save(mensaje);
-    }
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("unlimitededuconnect@gmail.com");
+        message.setTo(correosAdministradores.toArray(new String[0]));
+        message.setSubject("Nuevo asesor registrado en EduConnect");
+        message.setText("Se ha registrado un nuevo asesor:\n\n" +
+                "Nombre: " + asesor.getUsuario().getNombre() + "\n" +
+                "Correo electrónico: " + asesor.getUsuario().getCorreoElectronico() + "\n" +
+                "Especialidad: " + asesor.getEspecialidad() + "\n");
 
-    public void eliminarNotificacion(Integer idNotificacion) {
-    	notificacionRepository.deleteById(idNotificacion);
+        mailSender.send(message);
     }
 }
