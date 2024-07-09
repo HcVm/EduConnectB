077412d566b63c02a83022238a351a6f5335b32a
HcVm <hecvega96@outlook.com>
2024-07-09 11:28:27 -0500

    aer

--- src/main/java/com/EduConnectB/app/services/NotificacionService.java
+++ src/main/java/com/EduConnectB/app/services/NotificacionService.java
@@ -1,10 +1,32 @@
 package com.EduConnectB.app.services;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.springframework.stereotype.Service;
+
+import com.EduConnectB.app.models.Usuario;
 
 @Service
 public class NotificacionService {
+	
+	private final Map<Integer, List<String>> notificacionesPorUsuario = new ConcurrentHashMap<>();
 
+    public void enviarNotificacion(Usuario usuario, String mensaje) {
+        List<String> notificaciones = notificacionesPorUsuario.getOrDefault(usuario.getIdUsuario(), new ArrayList<>());
+        notificaciones.add(mensaje);
+        notificacionesPorUsuario.put(usuario.getIdUsuario(), notificaciones);
+    }
 
+    public List<String> obtenerNotificaciones(Usuario usuario) {
+        return notificacionesPorUsuario.getOrDefault(usuario.getIdUsuario(), Collections.emptyList());
+    }
+
+    public void marcarNotificacionesComoLeidas(Usuario usuario) {
+        notificacionesPorUsuario.remove(usuario.getIdUsuario());
+    }
     
 }
