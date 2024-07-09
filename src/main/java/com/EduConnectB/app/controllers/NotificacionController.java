a0ef43b625c5bda36493cfe5029a90f6365a97e1
HcVm <hecvega96@outlook.com>
2024-07-09 11:54:02 -0500

    topic

--- src/main/java/com/EduConnectB/app/controllers/NotificacionController.java
+++ src/main/java/com/EduConnectB/app/controllers/NotificacionController.java
@@ -7,17 +7,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.EduConnectB.app.models.Usuario;
 import com.EduConnectB.app.services.NotificacionService;
 import com.EduConnectB.app.services.UsuarioService;
 
-@Controller
+@RestController
+@RequestMapping("/notiftopic")
 public class NotificacionController {
 	
 	@Autowired
