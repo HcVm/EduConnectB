b905f17dee9fa2ba4b15c6afc66e567e3c939219
HcVm <hecvega96@outlook.com>
2024-07-08 20:59:54 -0500

    NotificacionesPruebas!

---
+++ src/main/java/com/EduConnectB/app/controllers/NotificacionController.java
@@ -0,0 +1,22 @@
+package com.EduConnectB.app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import com.EduConnectB.app.models.Usuario;
+
+@Controller
+public class NotificacionController {
+	
+	@Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/notificaciones") // Endpoint para recibir mensajes
+    public void enviarNotificacion(@Payload String mensaje, Usuario usuario) {
+        messagingTemplate.convertAndSendToUser(usuario.getCorreoElectronico(), "/topic/notificaciones", mensaje); // Envía la notificación al usuario específico
+    }
+
+}
