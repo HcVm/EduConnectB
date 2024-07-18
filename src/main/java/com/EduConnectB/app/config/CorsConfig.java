da7fe95bfc476818681e56568509d4a5b911e6aa
HcVm <hecvega96@outlook.com>
2024-07-18 18:11:49 -0500

    LTD F

--- src/main/java/com/EduConnectB/app/config/CorsConfig.java
+++ src/main/java/com/EduConnectB/app/config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
--- src/main/java/com/EduConnectB/app/controllers/SesionController.java
+++ src/main/java/com/EduConnectB/app/controllers/SesionController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
