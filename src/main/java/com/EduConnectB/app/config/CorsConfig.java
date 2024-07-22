dc0f96e818e878b7573d6ea29df409b366eea1ec
HcVm <hecvega96@outlook.com>
2024-07-22 03:26:18 -0500

    cors
    migracion CORS

--- src/main/java/com/EduConnectB/app/config/CorsConfig.java
+++ src/main/java/com/EduConnectB/app/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") 
+                        .allowedOrigins("https://educonnect-web.pages.dev/") 
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                 		.allowCredentials(true);
