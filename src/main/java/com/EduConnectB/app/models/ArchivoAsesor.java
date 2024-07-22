2b23dc7da72bcebcdc19cac3d334dc3443d66d4a
HcVm <hecvega96@outlook.com>
2024-07-22 02:01:53 -0500

    ltd
    g1-1

---
+++ src/main/java/com/EduConnectB/app/models/ArchivoAsesor.java
@@ -0,0 +1,62 @@
+package com.EduConnectB.app.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "archivos_asesor")
+public class ArchivoAsesor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_asesor", nullable = false)
+    private Asesor asesor;
+
+    private String nombreArchivo;
+    private String rutaArchivo;
+    private LocalDateTime fechaSubida;
+    
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public Asesor getAsesor() {
+		return asesor;
+	}
+	public void setAsesor(Asesor asesor) {
+		this.asesor = asesor;
+	}
+	public String getNombreArchivo() {
+		return nombreArchivo;
+	}
+	public void setNombreArchivo(String nombreArchivo) {
+		this.nombreArchivo = nombreArchivo;
+	}
+	public String getRutaArchivo() {
+		return rutaArchivo;
+	}
+	public void setRutaArchivo(String rutaArchivo) {
+		this.rutaArchivo = rutaArchivo;
+	}
+	public LocalDateTime getFechaSubida() {
+		return fechaSubida;
+	}
+	public void setFechaSubida(LocalDateTime fechaSubida) {
+		this.fechaSubida = fechaSubida;
+	}
+	public ArchivoAsesor(Asesor asesor, String nombreArchivo, String rutaArchivo, LocalDateTime fechaSubida) {
+		this.asesor = asesor;
+		this.nombreArchivo = nombreArchivo;
+		this.rutaArchivo = rutaArchivo;
+		this.fechaSubida = fechaSubida;
+	}
+	public ArchivoAsesor() {
+
+	}
+
+}
--- src/main/java/com/EduConnectB/app/models/Asesor.java
+++ src/main/java/com/EduConnectB/app/models/Asesor.java
@@ -1,6 +1,9 @@
 package com.EduConnectB.app.models;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -20,39 +23,58 @@ public class Asesor {
     @Column(columnDefinition = "json")
     private String horarioDisponibilidad;
     
+    @OneToMany(mappedBy = "asesor", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ArchivoAsesor> archivos = new ArrayList<>();
+
 	public Integer getIdAsesor() {
 		return idAsesor;
 	}
+
 	public void setIdAsesor(Integer idAsesor) {
 		this.idAsesor = idAsesor;
 	}
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
+
 	public String getEspecialidad() {
 		return especialidad;
 	}
+
 	public void setEspecialidad(String especialidad) {
 		this.especialidad = especialidad;
 	}
+
 	public String getHorarioDisponibilidad() {
 		return horarioDisponibilidad;
 	}
+
 	public void setHorarioDisponibilidad(String horarioDisponibilidad) {
 		this.horarioDisponibilidad = horarioDisponibilidad;
 	}
-	public Asesor(Usuario usuario, String especialidad, String horarioDisponibilidad) {
+
+	public List<ArchivoAsesor> getArchivos() {
+		return archivos;
+	}
+
+	public void setArchivos(List<ArchivoAsesor> archivos) {
+		this.archivos = archivos;
+	}
+
+	public Asesor(Usuario usuario, String especialidad, String horarioDisponibilidad, List<ArchivoAsesor> archivos) {
 		this.usuario = usuario;
 		this.especialidad = especialidad;
 		this.horarioDisponibilidad = horarioDisponibilidad;
+		this.archivos = archivos;
 	}
+
 	public Asesor() {
 
-
 	}
 
-    
 }
