a217b132b9acdbe4a9a8467e3626a1d024d2f2e5
HcVm <hecvega96@outlook.com>
2024-07-28 18:04:55 -0500

    c1
    c1

--- src/main/java/com/EduConnectB/app/controllers/NotificacionController.java
+++ src/main/java/com/EduConnectB/app/controllers/NotificacionController.java
@@ -1,23 +1,36 @@
 package com.EduConnectB.app.controllers;
+
+import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
-import com.EduConnectB.app.models.Usuario;
+import com.EduConnectB.app.dao.NotificacionRepository;
+import com.EduConnectB.app.models.Notificacion;
 
+import java.util.List;
 
 @RestController
-@RequestMapping("/ws")
+@RequestMapping("/notificaciones")
 public class NotificacionController {
-	
-	@Autowired
-    private SimpMessagingTemplate messagingTemplate;
-	
-	@MessageMapping("/notificaciones")
-    public void enviarNotificacion(@Payload String mensaje, Usuario usuario) {
-        messagingTemplate.convertAndSendToUser(usuario.getCorreoElectronico(), "/topic/notificaciones", mensaje);
+
+    @Autowired
+    private NotificacionRepository notificacionRepository;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<Notificacion>> getNotificaciones(@PathVariable Integer userId) {
+        List<Notificacion> notificaciones = notificacionRepository.findByUsuarioIdUsuario(userId);
+        return ResponseEntity.ok(notificaciones);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Notificacion> marcarComoLeida(@PathVariable Integer id) {
+        Notificacion notificacion = notificacionRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.SC_NOT_FOUND, "Notificación no encontrada", null));
+        
+        notificacion.setLeido(true);
+        Notificacion notificacionActualizada = notificacionRepository.save(notificacion);
+        return ResponseEntity.ok(notificacionActualizada);
     }
 }
