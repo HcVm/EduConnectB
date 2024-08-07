bfd0caf3c269cf12d287804d1e98172136c333a6
HcVm <hecvega96@outlook.com>
2024-08-06 21:24:18 -0500

    cred2.0
    cred2.0

--- src/main/java/com/EduConnectB/app/services/BackupService.java
+++ src/main/java/com/EduConnectB/app/services/BackupService.java
@@ -1,58 +1,39 @@
 package com.EduConnectB.app.services;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Types;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Collections;
-import java.util.List;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.api.client.http.FileContent;
+import com.google.api.services.drive.model.File;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.services.drive.DriveScopes;
-
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.FileContent;
-
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.model.File;
-
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Collections;
 
 @Service
 public class BackupService {
-	
-	private static final Logger logger = LoggerFactory.getLogger(BackupService.class);
+    
+    private static final Logger logger = LoggerFactory.getLogger(BackupService.class);
 
     private static final String APPLICATION_NAME = "EduConnect BackupApp";
-    private static final GsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
-    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_FILE);
+    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private static final String CREDENTIALS_FILE_PATH = "/etc/secrets/keysgdrive";
 
     private String railwayDbHost = "viaduct.proxy.rlwy.net";
@@ -85,7 +66,6 @@ public class BackupService {
 
                 while (tables.next()) {
                     String tableName = tables.getString("TABLE_NAME");
-
                     
                     if (tableName.equals("sys_config")) {
                         continue; 
@@ -144,8 +124,10 @@ public class BackupService {
 
     private void uploadToDrive(String filePath) {
         try {
-            final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+            GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(CREDENTIALS_FILE_PATH))
+                    .createScoped(Collections.singleton(DriveScopes.DRIVE_FILE));
+            HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+            Drive service = new Drive.Builder(httpTransport, JSON_FACTORY, new HttpCredentialsAdapter(credentials))
                     .setApplicationName(APPLICATION_NAME)
                     .build();
 
@@ -159,27 +141,9 @@ public class BackupService {
                     .setFields("id")
                     .execute();
 
-            System.out.println("File ID: " + file.getId());
-        } catch (IOException | GeneralSecurityException e) {
-            e.printStackTrace();
+            logger.info("File uploaded to Drive. File ID: {}", file.getId());
+        } catch (IOException | java.security.GeneralSecurityException e) {
+            logger.error("Error al subir el archivo a Google Drive", e);
         }
-    }
-
-    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        java.io.File credentialsFile = new java.io.File(CREDENTIALS_FILE_PATH);
-        if (!credentialsFile.exists()) {
-            throw new FileNotFoundException("El archivo de credenciales no se encontró en " + CREDENTIALS_FILE_PATH);
-        }
-
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
-                JSON_FACTORY, new InputStreamReader(new FileInputStream(credentialsFile)));
-
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 }
